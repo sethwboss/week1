db8e8e031e0d85000ea23cf5e42668671ee766fc
sethwboss <sethwboss@gmail.com>
2025-09-27 17:38:17 -0600

    Finished NPS part 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,54 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 
 const parkData = getParkData();
 
 
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
 
 
 
 
-function parkInfoTemplate(info) {
-  return `<a href="/" class="hero-banner__title">${info.name}</a>
-  <p class="hero-banner__subtitle">
-    <span>${info.designation}</span>
-    <span>${info.states}</span>
-  </p>`;
+
+
+
+
+
+
+
+
+
+function setIntro(data) {
+  const introSection = document.querySelector("#intro")
+  introSection.innerHTML = `<h1>${data.fullName}</h1> 
+  <p>${data.description}</p>`;
 }
 
 
 
-document.querySelector("head > title").textContent = parkData.fullName;
 
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
 
-document.querySelector(".hero-banner__content").innerHTML =
-  parkInfoTemplate(parkData);
+
+
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector("#info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
+  const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
+
+
+
+
+setHeaderFooter(parkData);
+setIntro(parkData)
+setParkInfoLinks(parkInfoLinks);
+
+
+
+
+
+
+
+
