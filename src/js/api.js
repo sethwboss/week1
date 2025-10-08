4df6ff770dc7c6eda78e8f298357932b954b8f02
sethwboss <sethwboss@gmail.com>
2025-10-08 16:25:45 -0600

    Finished Ponder activity, api html and api js

---
+++ src/js/api.js
@@ -0,0 +1,51 @@
+
+const outputList = document.getElementById('outputList');
+
+
+
+
+
+const baseUrl = "https://developer.nps.gov/api/v1/";
+
+async function getJson(endpoint) {
+  // replace this with your actual key
+  const apiKey = "5MmbdN0XVeoA0fJwZmamU8gJb9lDfBmQjJ5s79qP";
+
+  // construct the url: baseUrl + endpoint + parameters
+  const url = baseUrl + endpoint;
+
+  // set the options. The important one here is the X-Api-Key
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": apiKey
+      }
+  }
+  // make the request
+  const response = await fetch(url, options)
+  const data = await response.json()
+  console.log(data)
+  return data;
+}
+
+
+
+
+function listTemplate(item) {
+    return `<li><a href="${item.url}">${item.fullName}</a> ${item.states}</li>`
+}
+
+async function renderClimbingList() {
+  const endpoint = "activities/parks?q=climbing"
+  const listElement = document.getElementById("outputList")
+  const data = await getJson(endpoint)
+  const parks = data.data
+  const listHtml = parks.map(listTemplate).join("")
+  listElement.innerHTML = listHtml;
+}
+
+
+
+renderClimbingList();
+getJson('activities/parks?q=climbing');
